7e4011903deedcc847099a485face5129546ee9b
matteopstal <matteo.opstal@gmail.com>
2023-01-17 18:21:52 -0300

    tpu address

--- REDACTED/tf2_bert/bert_benchmark.py
+++ REDACTED/tf2_bert/bert_benchmark.py
@@ -140,7 +140,7 @@ class BertClassifyBenchmarkReal(PerfZeroBenchmark):
     if FLAGS.tpu:
       strategy = distribution_utils.get_distribution_strategy(
           distribution_strategy=FLAGS.distribution_strategy,
-          tpu_address="",
+          tpu_address="berttpu-v3-64",
           tpu_zone="europe-west4-a")
     else:
       strategy = distribution_utils.get_distribution_strategy(
